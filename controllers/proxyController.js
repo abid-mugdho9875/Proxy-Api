d82343c9b5a6d376e7392708e04c6879a816ba32
abid <abidmugdho@gmail.com>
2023-11-14 22:55:59 +0600

    making dynamic

--- controllers/proxyController.js
+++ controllers/proxyController.js
@@ -4,9 +4,27 @@ const cacheMiddleware = require('../middlewares/cacheMiddleware');
 class ProxyController {
   static async proxyRequest(req, res) {
     try {
+      // Destructure parameters from the request query
+      const {
+        referenceCurrencyUuid,
+        timePeriod,
+        tiers,
+        orderBy,
+        orderDirection,
+        limit,
+        offset,
+      } = req.query;
       // caching middleware apply here to store data for specefic time and limit the request time
       await cacheMiddleware(req, res, async () => {
-        const data = await ProxyModel.fetchData();
+        const data = await ProxyModel.fetchData({
+          referenceCurrencyUuid,
+          timePeriod,
+          tiers,
+          orderBy,
+          orderDirection,
+          limit,
+          offset,
+        });
         res.status(200).json(data);
       });
     } catch (error) {
--- models/proxyModel.js
+++ models/proxyModel.js
@@ -1,19 +1,20 @@
 const axios = require('axios');
 
 class ProxyModel {
-  static async fetchData() {
+  static async fetchData(params) {
     const options = {
       method: 'GET',
       url: 'https://coinranking1.p.rapidapi.com/coins',
-      params: {
-        referenceCurrencyUuid: 'yhjMzLPhuIDl',
-        timePeriod: '24h',
-        'tiers[0]': '1',
-        orderBy: 'marketCap',
-        orderDirection: 'desc',
-        limit: '50',
-        offset: '0'
-      },
+      params,
+      // params: {
+      //   referenceCurrencyUuid: 'yhjMzLPhuIDl',
+      //   timePeriod: '24h',
+      //   'tiers[0]': '1',
+      //   orderBy: 'marketCap',
+      //   orderDirection: 'desc',
+      //   limit: '50',
+      //   offset: '0'
+      // },
       headers: {
         'X-RapidAPI-Key': '03f8490efcmsh81fdb3742186abep123901jsn758ea15cb04a',
         'X-RapidAPI-Host': 'coinranking1.p.rapidapi.com'
